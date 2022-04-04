0c59d811dc7fb8ecc6eaa4d50f888153036cf6d3
developermkarim <m.karimcu@gmail.com>
2022-04-05 00:37:22 +0600

    3 reviewers in home set up

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -4,7 +4,11 @@ const About = () => {
     return (
         <div>
             <h3>About Us</h3>
-            <p>I am Mahmodul karim</p>
+            <p>Coffee Circus will provide a friendly, comfortable atmosphere where the customer can receive quality food, service and entertainment at a reasonable price. The coffee house will offer a variety of choices to the customers. Coffee and tea of all sorts will be offered. Juice, soda, and non-alcoholic beverages also will be available. Both wine and beer will be on sale. Coffee Circus will serve breakfast, lunch and dinner.
+<p>There will be nightly entertainment featuring acoustic jazz, blues and folk music. On selected nights there will be poetry readings and an open microphone. The walls will be used as an art gallery and from time to time there will be an artist in residence.</p>
+
+<p>The site contains a 3,525 square foot building which was used as a sports bar and restaurant. The building has substantial parking. The site shares the Hart Plaza with Folk Limited and Ryan Sports. Remodeling will consist of removing the existing booths, new carpet and some new furniture. The kitchen and bathrooms will require only minimum remodeling. The property is currently zoned for restaurants.</p>
+</p>
         </div>
     );
 };
--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,32 +1,41 @@
 import React from 'react';
 import useReviews from '../../Hooks/useReview';
 import BannerImg from '../../assets/images/coffe-banner.jpg'
+import { useNavigate } from 'react-router-dom';
+import { Card } from 'react-bootstrap';
 
 const Home = () => {
+    const navigation = useNavigate();
     const [Reviews, SetReviews] = useReviews();
     return (
-        <div>
-           <div className="banner-part">
-               <div className="banner-text">
+        <div className='container'>
+           <div className="banner-part m-auto d-flex mt-5 ">
+               <div className="banner-text col-8 mt-4 ms-">
                <h1>One Stop Cafe</h1>
-               <p>One Stop Cafe mission is to provide the highest quality food and beverage especially the coffee market for a reasonable price and homemade products. Our cafe wants to be in the business of helping the customers to relieve their daily stresses activity by providing piece of mind through great ambience, convenient location and the best customer service. The Cafe also has a mission and plan to provide job opportunities for community especially to local people.</p>
+               <p>One Stop Cafe mission is to provide the highest quality food and beverage especially the coffee market for a reasonable price and homemade products. Our cafe wants to be in the business of helping the customers to relieve their daily stresses activity by providing piece of mind through great ambience, convenient location and the best customer service.</p>
+               <button onClick={()=>navigation('/aboutUs')} className='btn btn-warning pointer' style={{ background: '#a65000',color:'white'}}>Learn More</button>
                </div>
-              <div className="bannerimg">
-                  <img src={BannerImg} alt="" />
+              <div className="bannerimg col-4">
+                  <img src={BannerImg} alt="" width={300} height={300}/>
               </div>
            </div>
 
             <div className="review-container">
+                <h3>Reviews</h3>
+                <div className="reviewer d-flex">
+
           {
               Reviews.slice(0,3).map(comment => 
-              <div>
+              <Card className='Card text-center'>
                   <div className="img">
-                      <img src={comment.img} alt="" />
+                      <img src={comment.img} alt="" width={100} height={100}/>
                   </div>
-                  <h1>{comment.name}</h1>
-              </div>
+                  <h4>{comment.name}</h4>
+                  <p>{comment.comment}</p>
+              </Card>
               )
           }
+          </div>
            
             </div>
         </div>
